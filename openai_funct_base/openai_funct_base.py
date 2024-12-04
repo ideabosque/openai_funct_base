58f2562896cc820d5ca421d13c02ba2743b4e6ca
Idea Bosque <ideabosque@gmail.com>
2024-12-04 09:59:08 -0800

    Update code style.

--- openai_funct_base/openai_funct_base.py
+++ openai_funct_base/openai_funct_base.py
@@ -1,66 +1,101 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from __future__ import print_function
+from __future__ import annotations
 
 __author__ = "bibow"
 
 import logging
 import traceback
-from typing import Any, Dict, List
+from typing import Any, Dict, Optional
 
 import boto3
+from botocore.exceptions import BotoCoreError, NoCredentialsError
 
 from silvaengine_utility import Utility
 
 
 class OpenAIFunctBase:
-    def __init__(self, logger: logging.Logger, **setting: Dict[str, Any]):
+    def __init__(self, logger: logging.Logger, **settings: Dict[str, Any]):
+        """
+        Initialize the OpenAIFunctBase class.
+        :param logger: Logger instance for logging errors and information.
+        :param settings: Configuration settings for AWS credentials and region.
+        """
         try:
-            # Set up AWS credentials in Boto3
             self.logger = logger
-            self.setting = setting
-            if (
-                setting.get("region_name")
-                and setting.get("aws_access_key_id")
-                and setting.get("aws_secret_access_key")
-            ):
-                self.aws_lambda = boto3.client(
-                    "lambda",
-                    region_name=setting.get("region_name"),
-                    aws_access_key_id=setting.get("aws_access_key_id"),
-                    aws_secret_access_key=setting.get("aws_secret_access_key"),
-                )
-            else:
-                self.aws_lambda = boto3.client(
-                    "lambda",
-                )
+            self.settings = settings
+            self._initialize_aws_lambda_client()
+        except (BotoCoreError, NoCredentialsError) as boto_error:
+            self.logger.error(f"AWS Boto3 error: {boto_error}")
+            raise boto_error
         except Exception as e:
             log = traceback.format_exc()
             self.logger.error(log)
             raise e
 
+    def _initialize_aws_lambda_client(self):
+        """
+        Initialize the AWS Lambda client using the provided credentials or default configuration.
+        """
+        region_name = self.settings.get("region_name")
+        aws_access_key_id = self.settings.get("aws_access_key_id")
+        aws_secret_access_key = self.settings.get("aws_secret_access_key")
+
+        if region_name and aws_access_key_id and aws_secret_access_key:
+            self.aws_lambda = boto3.client(
+                "lambda",
+                region_name=region_name,
+                aws_access_key_id=aws_access_key_id,
+                aws_secret_access_key=aws_secret_access_key,
+            )
+        else:
+            self.aws_lambda = boto3.client("lambda")
+
     def execute_graphql_query(
         self,
         endpoint_id: str,
-        funct: str,
+        function_name: str,
         query: str,
-        variables: Dict[str, Any] = {},
+        variables: Optional[Dict[str, Any]] = None,
     ) -> Dict[str, Any]:
+        """
+        Execute a GraphQL query using an AWS Lambda function.
+        :param endpoint_id: The ID of the GraphQL endpoint.
+        :param function_name: The name of the AWS Lambda function to invoke.
+        :param query: The GraphQL query string.
+        :param variables: Optional GraphQL variables.
+        :return: Response data from the GraphQL query.
+        :raises Exception: If there is an error in the GraphQL response.
+        """
+        if variables is None:
+            variables = {}
+
         params = {
             "query": query,
             "variables": variables,
         }
 
-        result = Utility.invoke_funct_on_aws_lambda(
-            self.logger,
-            self.aws_lambda,
-            **{"endpoint_id": endpoint_id, "funct": funct, "params": params},
-        )
-        result = Utility.json_loads(Utility.json_loads(result))
-        if result.get("data"):
-            return result["data"]
-        if result.get("errors"):
-            raise Exception(result["errors"])
-        if result.get("message"):
-            raise Exception(result["message"])
-        raise Exception(f"Unknown error: {result}")
+        try:
+            result = Utility.invoke_funct_on_aws_lambda(
+                self.logger,
+                self.aws_lambda,
+                **{
+                    "endpoint_id": endpoint_id,
+                    "funct": function_name,
+                    "params": params,
+                },
+            )
+            result = Utility.json_loads(Utility.json_loads(result))
+
+            if "data" in result:
+                return result["data"]
+            elif "errors" in result:
+                raise Exception(result["errors"])
+            elif "message" in result:
+                raise Exception(result["message"])
+            else:
+                raise Exception(f"Unknown error: {result}")
+
+        except Exception as e:
+            self.logger.error(f"Error executing GraphQL query: {e}")
+            raise
